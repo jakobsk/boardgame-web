57cc064a2d3ed5015ee027af80c76868e0d33823
jakobsk <jakobskok95@gmail.com>
2022-09-24 14:54:02 +0200

    Refactor to use useForm component instead

--- components/register-form.tsx
+++ components/register-form.tsx
@@ -1,26 +1,41 @@
-import { useEffect, useState } from "react";
-import styles from "../styles/RegisterForm.module.css";
-import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 
-// should also get this value form backend
-enum players {
-  JAKOB = "Jakob Stenersen Kok",
-  VEBJORN = "Vebjørn Nordahl Stadsvoll",
-  MATHIAS = "Mathias Ackermann",
-  ARVID = "Arvid Mildner",
-}
+import styles from '../styles/RegisterForm.module.css';
+import Button from 'react-bootstrap/Button';
+import { noParamNoCallback } from '../types/function-types';
 
 //TODO: This number should be fetched from backend maybe since every game has different allowed players
 
 const maxPlayers = 6;
 
-function RankingForm() {
-  const [inputFields, setInputFields] = useState([{ name: "", age: "" }]);
+function RankingForm({ toggleModal }: { toggleModal: noParamNoCallback }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [inputFields, setInputFields] = useState([{}]);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const onSubmit = (data) => {
+    console.log(typeof data);
+    toggleModal();
+    console.log(data);
+  };
 
   const removePlayer = () => {
     if (inputFields.length > 1) {
-      setInputFields([...inputFields.slice(0,inputFields.length-1)]);
+      setInputFields([...inputFields.slice(0, inputFields.length - 1)]);
+    }
+  };
+
+  const addPlayer = () => {
+    if (inputFields.length < maxPlayers) {
+      setInputFields([...inputFields, {}]);
+    } else {
+      setShowErrorMessage(true);
     }
   };
 
@@ -30,31 +45,59 @@ function RankingForm() {
     }
   }, [inputFields]);
 
-  const addPlayer = () => {
-    if (inputFields.length < maxPlayers) {
-      setInputFields([...inputFields, { name: "", age: "" }]);
-    }else {
-      setShowErrorMessage(true);
-    }
-  };
-
   return (
     <div className="App">
-      <form>
-        {inputFields.map((input, index) => {
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {inputFields.map((_, index) => {
+          const elementName = `name${index}`;
+          const elementRank = `rank${index}`;
           return (
-            <div key={index} className={styles.inputField}>
-              <h5 className={styles.playerNumber}>Spiller {index}: </h5>
-              <input name="name" placeholder="Navn" value={input.name} className={styles.formInput}/>
-              <input name="age" placeholder="Plassering" value={input.age}  className={styles.formInput}/>
-              <hr></hr>
-            </div>
+            <>
+              <div key={index} className={styles.inputField}>
+                <label htmlFor={elementName} className={styles.playerNumber}>
+                  Spiller {index + 1}:
+                </label>
+                <input
+                  type="text"
+                  id={elementName}
+                  placeholder="Navn på spiller"
+                  {...register(elementName, { required: 'Påkrevd' })}
+                  className={styles.formInput}
+                />
+                <input
+                  type="number"
+                  id={elementRank}
+                  placeholder="Plassering"
+                  {...register(elementRank, { required: 'Påkrevd' })}
+                  className={styles.formInput}
+                />
+
+                <hr></hr>
+              </div>
+              <div className={styles.validationErrors}>
+                {errors[elementName] && (
+                  <span className={styles.error}>Navn mangler</span>
+                )}
+                {errors[elementRank] && (
+                  <span className={styles.error}>Plassering mangler</span>
+                )}
+              </div>
+            </>
           );
         })}
+        <Button variant="dark" type="submit">
+          Registrer
+        </Button>
       </form>
 
-      <Button variant="dark" onClick={addPlayer}> Legg til spiller</Button>
-      <Button variant="dark" onClick={removePlayer}> Fjern spiller</Button>
+      <Button variant="dark" onClick={addPlayer}>
+        {' '}
+        Legg til spiller
+      </Button>
+      <Button variant="dark" onClick={removePlayer}>
+        {' '}
+        Fjern spiller
+      </Button>
       {showErrorMessage && (
         <div>Kan ikke legge til mer enn {maxPlayers} spillere</div>
       )}
